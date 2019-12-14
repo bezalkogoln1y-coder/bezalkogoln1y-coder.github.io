060ecebedbab21742aa78ad1d3170378f55c6749
homePC <muskatinartem@gmail.com>
2019-12-14 15:54:57 +0300

    Complete

--- template/js/scripts.js
+++ template/js/scripts.js
@@ -1,8 +1,11 @@
-function slickSlider() {
-    $('.slick-slider').each(function () {
+function slickSlider(element,countSlide,arrow) {
+    $(element).each(function () {
         $(this).slick({
             dots: false,
-            autoplay: false
+            autoplay: false,
+            arrows: arrow,
+            slidesToShow: countSlide,
+            slidesToScroll: countSlide
         });
     });
 }
@@ -27,13 +30,44 @@ function scrollTop() {
 function clickableLi(element) {
     $(element).click(function(e){
         e.preventDefault;
-        let link = $(this).children('a').attr('href');
+        let link = $(this).find('a.linken-sphere').attr('href');
         document.location.href = link;
     });
 }
 
+function changeModalName(element) {
+    $(element).click(function(){
+        let linkName = $(this).text(),
+            modal = $('#Modal'),
+            emailInput = $('#Modal p.email'),
+            messageInput = $('#Modal p.message'),
+            modalName = $('#Modal h3'),
+            modalTitle = $('#Modal p.subtitle');
+        modalName.text(linkName);
+        modalTitle.html('Оставляйте заявку!<br>Мы перезвоним вам в течении 10 минут');
+        emailInput.hide();
+        messageInput.hide();
+        if (linkName === 'Напишите нам') {
+            emailInput.show();
+            messageInput.show();
+            modalTitle.html('Заполняйте форму!<br>Мы ответим вам в течении 10 минут');
+        }
+        
+        if (linkName === 'Задать вопрос продавцу') {
+            emailInput.show();
+            messageInput.show();
+            modalTitle.html('Заполняйте форму!<br>Мы ответим вам в течении 10 минут');
+            messageInput.find('textarea').attr('placeholder', 'Ваш вопрос');
+        }
+    });
+}
+
 $(function(){
-    slickSlider();
+    $('.fancybox').fancybox();
+    slickSlider($('.slick-slider'),1,true);
+    slickSlider($('.slick-product'),4,true);
     scrollTop();
     clickableLi($('.left-menu .wrapper li.item'));
+    clickableLi($('.items.cat .product'));
+    changeModalName($('.modal-button a'));
 });
